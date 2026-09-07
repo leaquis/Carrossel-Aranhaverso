b0efb60d5b797443c9aceec88faa094a7a451ad7
Giovani da Silva Ramos <gi_1997@hotmail.com>
2026-09-06 23:18:42 -0300

    Refactor code structure for improved readability and maintainability

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,14 +1,17 @@
+import { spidermanFont } from "@/fonts";
+import { IHeroData } from "@/interfaces/heroes";
+import styles from "./heroesList.module.scss";
+
 interface IProps {
-  heroes: iHeroData[];
+  heroes: IHeroData[];
 }
 
 export default function HeroesList({ heroes }: IProps) {
   return (
     <>
-      <h1>Personagens</h1>
-      {heroes.map((hero) => (
-        <p key={hero.id}>{hero.name}</p>
-      ))}
+      <h1 className={`${spidermanFont.className} ${styles.title}`}>
+        Personagens
+      </h1>
     </>
   );
 }
